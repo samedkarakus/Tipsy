b433824ae3a5e72a7af5fbb1a06b39c075b798aa
samedkarakus <samedkarakus.ceng@gmail.com>
2024-08-04 01:10:30 +0300

    Initial Commit

---
+++ Tipsy/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Tipsy
+//
+//  Created by Samed Karakuş on 4.08.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
